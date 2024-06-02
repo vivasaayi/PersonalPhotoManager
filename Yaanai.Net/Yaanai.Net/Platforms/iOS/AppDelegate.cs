9df68bee84e14d9387f71f335bc06e62a284f113
Rajan Panneer Selvam <vivasaayi@live.com>
2024-06-02 10:04:58 -0400

    Initial Commit

---
+++ Yaanai.Net/Yaanai.Net/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Yaanai.Net;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
